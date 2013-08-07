c9a916135886fa673df9a7dbbd3ae8191f2c64fc
Adam Goff <adamlgoff@gmail.com>
2013-08-07 11:03:38 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,8 +25,8 @@ describe "UserPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Exampled Users"
-        fill_in "Email",        with: "user@exampled.com"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
